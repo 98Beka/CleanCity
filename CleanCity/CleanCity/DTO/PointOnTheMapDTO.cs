b9e557d1a889f1579c9c5f6663ec0c1763891a7b
Ahmedjanov Shahzod <shahmedjanov>
2023-03-12 03:43:45 +0600

    Add LikeValue to PointOnTheMapDTO

--- CleanCity/CleanCity/DTO/PointOnTheMapDTO.cs
+++ CleanCity/CleanCity/DTO/PointOnTheMapDTO.cs
@@ -9,6 +9,8 @@
         public string FIO { get; set; }
         public string Email { get; set; }
         public float Rating { get; set; }
+        public long LikeId { get; set; }
+        public int LikeValue { get; set; }
         public string Address { get; set; }
         public double[] Position { get; set; }
         public List<string> FilesBase64 { get; set; }
