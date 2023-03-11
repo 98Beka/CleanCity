aa2129031f374ba50a823ebc59e8e51fcd2dc926
Ahmedjanov Shahzod <shahmedjanov>
2023-03-11 15:15:01 +0600

    Update DataContext.cs

--- CleanCity/CleanCity/Data/DataContext.cs
+++ CleanCity/CleanCity/Data/DataContext.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CleanCity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanCity.Data
 {
     public class DataContext : IdentityDbContext<IdentityUser, IdentityRole, string> {
+
+        DbSet<PointOnTheMap> PointOnTheMaps { get; set; }
+        DbSet<Photo> Photos { get; set; }
+
         public DataContext(DbContextOptions<DataContext> options)
             : base(options) { }
         public DataContext() {
